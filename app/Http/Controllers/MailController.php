e401aedff2809c6417dc2104b2a9e75f1e0dc92f
irenejoy15 <34486370+irenejoy15@users.noreply.github.com>
2024-10-21 16:12:04 +0800

    Added no memory Limiy

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,7 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
-
+ini_set('memory_limit', '-1');
+ini_set('max_execution_time', 0);
+ini_set('sqlsrv.ClientBufferMaxKBSize','1000000'); // Setting to 512M
+ini_set('pdo_sqlsrv.client_buffer_max_kb_size','1000000');
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\EmailView;
